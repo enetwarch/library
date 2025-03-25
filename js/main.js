6c8324dd39a1926acece24d3da5a1f8741eedb51
Enetwarch <enetwarch@protonmail.com>
2025-03-25 22:00:56 +0800

    renamed sample to entries.json

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import sample from "./modules/sample.json" with { "type": "json" };
+import entries from "./modules/entries.json" with { "type": "json" };
 import Modal from "./modules/modal.js";
 import Library from "./modules/library.js";
 import Form from "./modules/form.js";
@@ -10,15 +10,15 @@ window.addEventListener("load", () => {
     const storedLibrary = localStorage.getItem("library");
     if (!storedLibrary || !(storedLibrary instanceof Library)) {
         const container = document.getElementById("output");
-        const entries = structuredClone(sample);
-        library = new Library(container, entries);
+        const sampleEntries = structuredClone(entries);
+        library = new Library(container, sampleEntries);
     } else {
         library = JSON.parse(storedLibrary);
         library.loadEntries();
     }
 
     library.addListener("click", event => {
-        
+
     });
 
     window.addEventListener("beforeunload", () => {
