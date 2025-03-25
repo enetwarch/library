301c0ffc3d300ca74b0310c04d9fd8a1f14faaad
Enetwarch <enetwarch@protonmail.com>
2025-03-25 17:22:16 +0800

    added local storage saving every unload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-import entries from "./modules/entries.json" with { "type": "json" };
+import sample from "./modules/sample.json" with { "type": "json" };
+import Modal from "./modules/modal.js";
 import Entry from "./modules/entry.js";
 
 window.addEventListener("load", () => {
     document.addEventListener("contextmenu", event => event.preventDefault());
 
+    let entries;
+    const storedEntries = localStorage.getItem("entries");
+    if (!storedEntries) {
+        entries = structuredClone(sample);
+    } else {
+        entries = JSON.parse(storedEntries);
+    }
     entries.forEach(entry => new Entry(entry));
+    window.addEventListener("beforeunload", () => {
+        localStorage.setItem("entries", JSON.stringify(entries));
+    });
 
     const modalForm = document.getElementById("modalForm");
     const newEntry = document.getElementById("newEntry");
@@ -21,9 +32,10 @@ window.addEventListener("load", () => {
         event.preventDefault();
 
         const formData = new FormData(entryForm);
-        const libraryEntry = new Entry(formData);
+        const entry = new Entry(formData);
+        entries.push(entry);
+
         entryForm.reset();
         modalForm.close();
     });
 });
-
